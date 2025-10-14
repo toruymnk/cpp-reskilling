ea6f69baa56aaac2c0ec937a44e38c3eb6b3a948
Toru Yamanaka <toruymnk@gmail.com>
2025-10-14 22:56:24 +0900

    Exercise 3.1: 2. overload with non-const reference and const reference

--- exercise3-1.cpp
+++ exercise3-1.cpp
@@ -1,15 +1,31 @@
+#include <iostream>
+
 class A {
   int v;
 
  public:
   void set(int value);
-  int get() const;  // 1. constメンバー関数にする。
+  // int get() const;         // 1. constメンバー関数にする。
+  int& get();              // 2. 非const参照を返すget()
+  const int& get() const;  // 2. const参照を返すget()
 };
 
 void A::set(int value) { v = value; }
 
-int A::get() const  // 1. constメンバー関数にする。
+// int A::get() const  // 1. constメンバー関数にする。
+// {
+//   return v;
+// }
+
+int& A::get()  // 2.
 {
+  std::cout << "非const参照を返すget()" << std::endl;
+  return v;
+}
+
+const int& A::get() const  // 2.
+{
+  std::cout << "const参照を返すget()" << std::endl;
   return v;
 }
 
@@ -17,6 +33,12 @@ int main() {
   A a;
   a.set(42);
 
-  const A& ca = a;
-  ca.get();
+  const A& cra = a;
+  cra.get();  // const参照を返す。
+
+  // 2.
+  a.get();  // 非const参照を返す。
+
+  const A ca{};
+  ca.get();  // const参照を返す。
 }
