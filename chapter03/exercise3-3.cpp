f2c7b62adf60c3068bad1b64443daeaa104678f7
Toru Yamanaka <me@toruymnk.dev>
2025-10-16 22:21:16 +0900

    Exercise 3.3: 1. Impliment constructor

--- chapter03/exercise3-3.cpp
+++ chapter03/exercise3-3.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 
 class Book {
@@ -7,6 +8,27 @@ class Book {
 
  public:
   Book(std::string title, std::string author, int price);
+
+  std::string Title() const;
+  std::string Author() const;
+  int Price() const;
 };
 
-int main() {}
+// 1.
+Book::Book(std::string title, std::string author, int price)
+    : title_(title), author_(author), price_(price) {}
+
+std::string Book::Title() const { return title_; }
+
+std::string Book::Author() const { return author_; }
+
+int Book::Price() const { return price_; }
+
+int main() {
+  // 1.
+  Book cpp("独習C++", "高橋航平", 3718);
+
+  std::cout << "タイトル: " << cpp.Title() << std::endl;
+  std::cout << "著者: " << cpp.Author() << std::endl;
+  std::cout << "価格: " << cpp.Price() << " 円" << std::endl;
+}
