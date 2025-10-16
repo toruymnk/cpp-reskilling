5f91fb5ff665b3eaeebab435eec12586da8708c7
Toru Yamanaka <me@toruymnk.dev>
2025-10-16 21:16:21 +0900

    3.2.3, 3.2.5: Rename with Google C++ Style Guide

--- 3-2-3.cpp
+++ 3-2-3.cpp
@@ -1,46 +1,46 @@
 #include <iostream>
 #include <string>
 
-class person {
+class Person {
   std::string name_;
   int age_;
 
  public:
-  person();
-  ~person();  // 3.2.5
+  Person();
+  ~Person();  // 3.2.5
 
-  void set_name(std::string name);
-  void set_age(int age);
+  void SetName(std::string name);
+  void SetAge(int age);
 
-  std::string name() const;
-  int age() const;
+  std::string Name() const;
+  int Age() const;
 };
 
 // 不正な値で年齢を初期化するコンストラクター。
 // name_はstd::stringのデフォルトで初期化される。
-person::person() : age_(-1) {
+Person::Person() : age_(-1) {
   std::cout << "コンストラクター呼び出し" << std::endl;
 }
 
 // 3.2.5
-// personのインスタンスを破棄するときに呼ばれるデストラクター。
+// Personのインスタンスを破棄するときに呼ばれるデストラクター。
 // デストラクターはオーバーロードできない。
-person::~person() { std::cout << "デストラクター呼び出し" << std::endl; }
+Person::~Person() { std::cout << "デストラクター呼び出し" << std::endl; }
 
-void person::set_name(std::string name) { name_ = name; }
+void Person::SetName(std::string name) { name_ = name; }
 
-void person::set_age(int age) { age_ = age; }
+void Person::SetAge(int age) { age_ = age; }
 
-std::string person::name() const { return name_; }
+std::string Person::Name() const { return name_; }
 
-int person::age() const { return age_; }
+int Person::Age() const { return age_; }
 
 int main() {
-  person alice;  // コンストラクターによる初期化が行われる。
+  Person alice;  // コンストラクターによる初期化が行われる。
 
-  std::cout << "初期化直後の年齢: " << alice.age() << std::endl;
-  alice.set_name("alice");
-  alice.set_age(30);
-  std::cout << "名前: " << alice.name() << std::endl;
-  std::cout << "年齢: " << alice.age() << std::endl;
+  std::cout << "初期化直後の年齢: " << alice.Age() << std::endl;
+  alice.SetName("alice");
+  alice.SetAge(30);
+  std::cout << "名前: " << alice.Name() << std::endl;
+  std::cout << "年齢: " << alice.Age() << std::endl;
 }
