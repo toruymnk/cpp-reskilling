f933a46dffbaeee1ac4b3d4cedc107852db5c6e1
Toru Yamanaka <me@toruymnk.dev>
2025-10-16 22:12:42 +0900

    Exercise 3.3

---
+++ chapter03/exercise3-3.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+class Book {
+  std::string title_;
+  std::string author_;
+  int price_;
+
+ public:
+  Book(std::string title, std::string author, int price);
+};
+
+int main() {}
