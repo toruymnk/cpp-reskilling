0cc36231c6aa59d7f663d9514e33793b1b2ba14d
Toru Yamanaka <me@toruymnk.dev>
2025-10-17 22:29:11 +0900

    Exercise 3.4: 1. NSDMI

--- chapter03/exercise3-4.cpp
+++ chapter03/exercise3-4.cpp
@@ -1,12 +1,24 @@
+#include <iostream>
+
 class Vector3D {
-  float x_;
-  float y_;
-  float z_;
+  float x_ = 12;  // 1.
+  float y_ = 34;  // 1.
+  float z_ = 56;  // 1.
 
  public:
-  Vector3D();
+  // Vector3D();
+
+  void Show() const;
 };
 
-Vector3D::Vector3D() : x_(0), y_(0), z_(0) {}
+// Vector3D::Vector3D() : x_(0), y_(0), z_(0) {}
 
-int main() {}
+void Vector3D::Show() const {
+  std::cout << "x_: " << x_ << ", y_: " << y_ << ", z_: " << z_ << std::endl;
+}
+
+int main() {
+  Vector3D v3d;
+
+  v3d.Show();
+}
